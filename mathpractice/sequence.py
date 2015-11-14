f6dcdfdc974ce13de251d419a2c8b39f85767357
Juan Reyero <juan@juanreyero.com>
2015-11-14 17:53:41 +0000

    Working sequence, more or less

--- mathpractice/sequence.py
+++ mathpractice/sequence.py
@@ -9,6 +9,7 @@ import numpy as np
 import random
 import time
 import pickle
+import os.path
 
 
 
@@ -18,13 +19,15 @@ import pickle
 def set_first_dic(fname):
     """Sets an empty diccionary in an empty
     file"""
-    pickle.dump({}, open(fname, "wb"))
+    with open(fname, 'wb') as fd:
+        pickle.dump({}, fd)
 
 
 def load_dic(fname):
     """Loads the pickle dic from a file.
     """
-    return pickle.load(open(fname, "rb"))
+    with open(fname, 'rb') as fd:
+        return pickle.load(open(fname, "rb"))
 
 
 def save_dic(key, pocket, fname):
@@ -34,7 +37,8 @@ def save_dic(key, pocket, fname):
 
     dic = load_dic(fname)
     dic[key] = pocket
-    pickle.dump(dic, open(fname, "wb"))
+    with open(fname, 'wb') as fd:
+        pickle.dump(dic, fd)
 
 
 def set_num_sequence(length, s_range):
@@ -75,41 +79,48 @@ def check_answer(sequence, answer):
     """
 
     try:
-        if [int(i) for i in answer.split(', ')] == sequence:
+        if [int(i) for i in answer.split()] == sequence:
             return True
-        return False
     except:
-        return False
+        pass
+    return False
 
-def ask_all(length, s_range, memorization_time=5, corrects=0, counter=0):
-    if counter == 10:
+
+def ask_all(length, s_range, n_questions, memorization_time=2, corrects=0, counter=0):
+    if counter == n_questions:
         return corrects
     sequence = set_num_sequence(length, s_range)
     answer = ask_num_sequence(sequence, memorization_time)
     if check_answer(sequence, answer):
-        return ask_all(length, s_range, memorization_time, corrects+1, counter+1)
-    return ask_all(length, s_range, memorization_time, corrects, counter+1)
+        return ask_all(length, s_range, n_questions,
+                       memorization_time, corrects+1, counter+1)
+    return ask_all(length, s_range, n_questions, memorization_time, corrects, counter+1)
 
 
-def check_dic():
-    last_score_dic = load_dic("sequence_scores.pkl")
+def check_dic(scores_file):
+    if not os.path.exists(scores_file):
+        set_first_dic(scores_file)
+    last_score_dic = load_dic(scores_file)
     if 'last score' not in last_score_dic:
         return None
     return last_score_dic['last score']
 
 
 def practice():
-    last_score = check_dic()
+    scores_file = "sequence_scores.pkl"
+    last_score = check_dic(scores_file)
     if last_score is None:
          last_score = {'length':3, 's_range':[1, 10], 'corrects':None}
-    corrects = ask_all(last_score['length'], last_score['s_range'])
-    if corrects == 10:
-        print "Well done, 10 out of 10! You get to the next level"
-        last_score[length] += 1
-        save_dic('last score', last_score, "sequence_scores.pkl")
+    n_questions = 2
+    corrects = ask_all(last_score['length'], last_score['s_range'], n_questions)
+    if corrects == n_questions:
+        print "Well done, %d out of %d! You get to the next level" % (n_questions,
+                                                                      n_questions)
+        last_score['length'] += 1
+        save_dic('last score', last_score, scores_file)
     else:
-        print "You have to get all ten correct to get to the next level"
-        save_dic('last score', last_score, "sequence_scores.pkl")
+        print "You have to get all %d correct to get to the next level" % n_questions
+        save_dic('last score', last_score, scores_file)
 
 
 
